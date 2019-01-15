270047ff635add40a00b09a93daf607713380922
Feng Li <fengggli@yahoo.com>
2019-01-15 12:28:58 -0500

    starting on fully connectednets

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -20,6 +20,7 @@ def svm_loss_naive(W, X, y, reg):
   - gradient with respect to weights W; an array of same shape as W
   """
   dW = np.zeros(W.shape) # initialize the gradient as zero
+  dW_add = np.zeros(W.shape)
 
   # compute the loss and the gradient
   num_classes = W.shape[1]
@@ -28,12 +29,20 @@ def svm_loss_naive(W, X, y, reg):
   for i in range(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
+    temp_dif = np.zeros(W.shape)
+
     for j in range(num_classes):
       if j == y[i]:
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
-      if margin > 0:
+      if margin > 0: # loss
         loss += margin
+
+    select = np.ones(num_classes) # add -X[i] to class with 
+    select[y[i]] = -1
+    add_item = np.outer(X[i], select)
+    #print(temp_dif[:10, :10])
+    dW_add = dW_add + add_item
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
@@ -41,6 +50,7 @@ def svm_loss_naive(W, X, y, reg):
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW = dW + 2*reg*W + dW_add/num_train
 
   #############################################################################
   # TODO:                                                                     #
