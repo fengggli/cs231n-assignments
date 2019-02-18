64c1230fcb828736a7ae3c6e263007ed410015d2
Feng Li <fengggli@yahoo.com>
2019-02-18 12:20:48 -0500

    update momentum and notebooks

--- assignment2/cs231n/data_utils.py
+++ assignment2/cs231n/data_utils.py
@@ -42,14 +42,15 @@ def load_CIFAR10(ROOT):
 
 
 def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000,
-                     subtract_mean=True):
+                     subtract_mean=True, data_dir='cs231n/datasets'):
     """
     Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
     it for classifiers. These are the same steps as we used for the SVM, but
     condensed to a single function.
     """
     # Load the raw CIFAR-10 data
-    cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+    #cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+    cifar10_dir = data_dir +'/cifar-10-batches-py'
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
     # Subsample the data
